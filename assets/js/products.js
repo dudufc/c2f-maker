73eabaf65a794b3d03ae0f1b03aafd96308184c7
Eduardo França <dudufranca121@gmail.com>
2026-09-07 18:42:37 -0300

    Adiciona fotos do seca-cuia e fecha-pacotes

--- assets/js/products.js
+++ assets/js/products.js
@@ -87,11 +87,13 @@ const PRODUTOS_C2F = [
     {
         nome: 'Seca-cuia',
         descricao: 'Suporte para deixar a cuia secar corretamente após o uso.',
-        preco: 'R$ 25,00'
+        preco: 'R$ 25,00',
+        imagem: 'assets/img/seca-cuia.png'
     },
     {
         nome: 'Fecha-pacotes',
         descricao: 'Acessório prático para manter embalagens bem fechadas.',
-        preco: 'R$ 25,00'
+        preco: 'R$ 25,00',
+        imagem: 'assets/img/fecha-pacotes.png'
     }
 ];
